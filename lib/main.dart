faa4a806adb8096d6fd20e1dba317cea2df1487e
Snehal Dhameliya <54747676+Snehal018@users.noreply.github.com>
2020-11-21 20:38:36 +0530

    Making True False Button WOrk

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,16 @@ class QuizPage extends StatefulWidget {
   _QuizPageState createState() => _QuizPageState();
 }
 
+int questionNumber = 0;
+
+List<String> questions = [
+  'You can lead a cow down stairs but not up stairs.',
+  'Approximately one quarter of human bones are in the feet.',
+  'A slug\'s blood is green.',
+];
+
+List<bool> answers = [false, true, true];
+
 List<Icon> scoreKeeper = [];
 
 void addTrue() {
@@ -58,7 +68,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -82,8 +92,15 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                bool correctAnswer = answers[questionNumber];
+
+                if (correctAnswer == true) {
+                  print('User Got it');
+                } else {
+                  print('user did not got it');
+                }
                 setState(() {
+                  questionNumber++;
                   addTrue();
                 });
               },
@@ -103,8 +120,14 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                bool correctAnswer = answers[questionNumber];
+                if (correctAnswer == false) {
+                  print('User Got it');
+                } else {
+                  print('user did not got it');
+                }
                 setState(() {
+                  questionNumber++;
                   addFalse();
                 });
               },
